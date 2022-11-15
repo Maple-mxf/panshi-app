d635610a868f8d3dcf77f771a5602a17ec50a1f0
maxuefeng <m17793873123@163.com>
2022-11-15 18:56:43 +0800

    add mapstruct

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,6 +95,9 @@ project("discovery-core") {
         implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
         implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
         implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+        implementation("org.mapstruct:mapstruct:1.5.3.Final")
+        annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Fina")
+        testAnnotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Fina")
     }
 }
 
